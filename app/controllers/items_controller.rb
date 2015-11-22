349b40e12f58799a540c8baecd948053e34fa6a4
nag1027 <nagnaomichi27@yahoo.co.jp>
2015-11-22 14:13:29 +0900

    クリエイトファイルを作成しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,8 +9,30 @@ def index
 
 end
 
+def new
+@item = Item.new
+
+end
+
+
+def create
+
+@item = Item.new(item_params)
+@item.save
+redirect_to "/items/#{@item.id}"
+
+end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
 
 
 
+
+
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+ get '/items/new' => 'items#new'
  get '/items/:id' => 'items#show'
  get '/items/' => 'items#index'
+ post '/items' => 'items#create'
+
 
 
 
